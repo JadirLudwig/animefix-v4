617dbd86f59da4ebb118919795a46877430710a0
JadirLudwig <jadir.ludwig@gmail.com>
2026-06-29 22:33:01 -0300

    Delete animefix-v3/animefix-v3 directory

--- animefix-v3/animefix-v3/test_parse2.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://meusanimes.blog/e/avatar-aang-the-last-airbender-2026/"
-resp = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-soup = BeautifulSoup(resp.text, 'lxml')
-
-print("Player Sist:")
-print(soup.select_one('.player_sist'))
